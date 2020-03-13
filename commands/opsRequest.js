0f3544a6669e32ed022fedee793cbd7ffcf891f0
nickmorell <nicholas@morell.dev>
2020-03-12 22:35:22 -0400

    Checking example rich embed

--- commands/opsRequest.js
+++ commands/opsRequest.js
@@ -5,11 +5,22 @@ module.exports = {
     description: 'Send a request to the Dev Ops team.',
     execute(message, args) {
 
-        const embed = new Discord.RichEmbed()
-            .setColor('#0099ff')
-            .settitle('New Request')
-            .setAuthor(message.author)
-            .setDescription(args[0]);
+        // inside a command, event listener, etc.
+    const embed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addField('Regular field title', 'Some value here')
+        .addBlankField()
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
 
         message.channel.send(embed);
