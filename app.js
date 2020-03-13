03ab8b39c0b427c91aaea5bb86c7d1a09c756e56
Nicholas Morell <nicholas@morell.dev>
2020-03-12 20:35:26 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const {prefix, token} = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Asher is ready and running!');
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return; // Return if bot or does not use prefix
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch(error) {
+        console.error(error);
+        message.reply("There was an error trying to execute the command!");
+    }
+
+});
+
+client.login(token);
